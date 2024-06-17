4954d98b3ea13e8017aa35577dd5b0eeb2ee0b1c
krjofficial <krishrjain.official@gmail.com>
2024-06-17 11:23:08 +0530

    hero-image

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,32 +1,59 @@
-import React from 'react'
-import './Navbar.css';
-import logo from '../Assets/logo.png';
-import cart_icon from '../Assets/cart_icon.png';
-import { useState } from 'react';
-
+import React from "react";
+import "./Navbar.css";
+import logo from "../Assets/logo.png";
+import cart_icon from "../Assets/cart_icon.png";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
   const [menu, setMenu] = useState("shop");
   return (
-      <div className="navbar">
-        <div className='nav-logo'>
-          <img src={logo} alt='logo' />
-          <p>E-Mart</p>
-        </div>
-        <ul className='nav-menu'>
-          <li onClick={()=>{setMenu("shop")}}>Shop {menu==="shop"?<hr/>:<></>}</li>
-          <li onClick={()=>{setMenu("men")}}>Men {menu==="men"?<hr/>:<></>}</li>
-          <li onClick={()=>{setMenu("women")}}>Women {menu==="women"?<hr/>:<></>}</li>
-          <li onClick={()=>{setMenu("kids")}}>Kids {menu==="kids"?<hr/>:<></>}</li>
-        </ul>
-        <div className='nav-login-cart'>
-          <button>Login</button>
-          <img src={cart_icon} alt='cart_icon' />
-          <div className="nav-cart-count">0</div>
-        </div>
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="logo" />
+        <p>E-Mart</p>
       </div>
-  )
-}
+      <ul className="nav-menu">
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          <Link  style={{ textDecoration: 'none'}} to="/">Shop</Link> {menu === "shop" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("men");
+          }}
+        >
+          <Link  style={{ textDecoration: 'none'}} to="/men">Men</Link> {menu === "men" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("women");
+          }}
+        >
+          <Link  style={{ textDecoration: 'none'}} to="/women">Women</Link> {menu === "women" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          <Link  style={{ textDecoration: 'none'}} to="/kids">Kids</Link> {menu === "kids" ? <hr /> : <></>}
+        </li>
+      </ul>
+      <div className="nav-login-cart">
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/cart">
+          <img src={cart_icon} alt="cart_icon" />
+        </Link>
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ frontend/src/Pages/ShopCategory.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShopCategory = () => {
+  return (
+    <div>
+      shop Category
+    </div>
+  )
+}
+
+export default ShopCategory
